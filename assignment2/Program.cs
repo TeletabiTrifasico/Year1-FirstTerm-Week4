862097e223bd0c8521b225bd3a86f18a38da5130
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 17:01:23 +0100

    Add files via upload

---
+++ assignment2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter target number: ");
+            int targetNumber = Convert.ToInt32(Console.ReadLine());
+            int counter = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number: ");
+                int numberGuess = Convert.ToInt32(Console.ReadLine());
+
+                if (numberGuess == targetNumber)
+                {
+                    counter++;
+                }
+                else if (numberGuess == 0)
+                {
+                    Console.WriteLine($"Count of numbers equal to target number: {counter}");
+                    break;
+                }
+            }
+        }
+    }
+}
