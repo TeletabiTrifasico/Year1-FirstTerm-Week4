29ec05998c2ae05048835a836080809aa8b57f70
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 17:02:30 +0100

    Add files via upload

---
+++ assignment4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            int one = 0;
+            int two = 1;
+
+            for (int i = 0; i < 20; i++)
+            {
+                one = two;
+                two = sum;
+                sum = one + two;
+                Console.Write($"{sum}, ");
+            }
+        }
+    }
+}
