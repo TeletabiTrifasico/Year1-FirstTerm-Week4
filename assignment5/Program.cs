309e5db0a486b5385ace29851b4e506ae9ba4c7b
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 17:03:50 +0100

    Add files via upload

---
+++ assignment5/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a year: ");
+                double year = double.Parse(Console.ReadLine());
+
+                if ((year % 4 == 0 || year % 400 == 0) && year % 100 != 0)
+                {
+                    Console.WriteLine($"{year} is a leap year.");
+                }
+                else if (year == 2000)
+                {
+                    Console.WriteLine($"{year} is a leap year.");
+                }
+                else if (year < 0)
+                {
+                    Console.WriteLine("Year must be positive!");
+                }
+                else if (year == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"{year} is not a leap year.");
+                }
+            }
+        }
+    }
+}
