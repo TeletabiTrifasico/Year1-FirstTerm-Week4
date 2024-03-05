bbae0129dd352665086297c330f19e334639d5cb
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 17:00:48 +0100

    Add files via upload

---
+++ assignment1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // set culture of program
+            CultureInfo ci = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = ci;
+            Thread.CurrentThread.CurrentCulture = ci;
+
+            int number = 1;
+            int amount = 0;
+            double sum = 0;
+
+            while (number != 0)
+            {
+                Console.WriteLine("Enter a number: ");
+                string input = Console.ReadLine();
+                number = Convert.ToInt32(input);
+                
+
+                if (number > 0)
+                {
+                    sum += number;
+                    amount++;
+                }
+                else if (number == 0)
+                {
+                    if (sum == 0)
+                    {
+                        Console.WriteLine($"Average of all positive numbers is: {sum:0.00}");
+                    }
+                    else
+                    {
+                        sum = sum / amount;
+                        Console.WriteLine($"Average of all positive numbers is: {sum:0.00}");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
