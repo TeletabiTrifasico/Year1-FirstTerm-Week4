76399c84a4fcfb7081655963348f4f975103d81f
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 17:01:53 +0100

    Add files via upload

---
+++ assignment3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fifthInput = 0;
+            int sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Enter a number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                fifthInput++;
+                if (fifthInput == 5)
+                {
+                    sum += number;
+                    fifthInput = 0;
+                }
+                else if (number == 0)
+                {
+                    Console.WriteLine($"Sum of 5th, 10th, 15th, ... number is: {sum}");
+                    break;
+                }
+            }
+        }
+    }
+}
